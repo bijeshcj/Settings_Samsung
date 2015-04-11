ae4035559b2da7f419b6b797a9f703b9dcaf4d59
unknown <bijesh.jayaprakash@gmail.com>
2015-04-11 12:44:45 +0530

    nekt: intro screen view pager and your network

--- app/src/main/java/com/bijesh/settings/android/uidesign/viewpager/ViewPagerAdapter.java
+++ app/src/main/java/com/bijesh/settings/android/uidesign/viewpager/ViewPagerAdapter.java
@@ -3,11 +3,13 @@ package com.bijesh.settings.android.uidesign.viewpager;
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.bijesh.settings.R;
@@ -15,59 +17,52 @@ import com.bijesh.settings.R;
 public class ViewPagerAdapter extends PagerAdapter {
 	// Declare Variables
 	Context context;
-	String[] rank;
-	String[] country;
-	String[] population;
-	int[] flag;
 	LayoutInflater inflater;
+    private int[] mDrawables;
+    private static final int YOUR_NETWORK = 0;
+    private static final int ONE_ADDRESS = 1;
+    private static final int WHO_MATTER = 2;
+    private static final int NOTIFICATIONS = 3;
 
-	public ViewPagerAdapter(Context context, String[] rank, String[] country,
-			String[] population, int[] flag) {
-		this.context = context;
-		this.rank = rank;
-		this.country = country;
-		this.population = population;
-		this.flag = flag;
-	}
+    ImageView imgflag;
+    TextView txtHeader;
+
+
+    public ViewPagerAdapter(Context context,int[] drawables){
+        this.context = context;
+        this.mDrawables = drawables;
+    }
 
 	@Override
 	public int getCount() {
-		return rank.length;
+		return mDrawables.length;
 	}
 
 	@Override
 	public boolean isViewFromObject(View view, Object object) {
-		return view == ((RelativeLayout) object);
+		return view == ((ScrollView) object);
 	}
 
 	@Override
 	public Object instantiateItem(ViewGroup container, int position) {
 
 		// Declare Variables
-		TextView txtrank;
-		TextView txtcountry;
-		TextView txtpopulation;
-		ImageView imgflag;
+
 
 		inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View itemView = inflater.inflate(R.layout.viewpager_item, container,
 				false);
 
-		// Locate the TextViews in viewpager_item.xml
-		txtrank = (TextView) itemView.findViewById(R.id.rank);
-		txtcountry = (TextView) itemView.findViewById(R.id.country);
-		txtpopulation = (TextView) itemView.findViewById(R.id.population);
-
-		// Capture position and set to the TextViews
-		txtrank.setText(rank[position]);
-		txtcountry.setText(country[position]);
-		txtpopulation.setText(population[position]);
 
 		// Locate the ImageView in viewpager_item.xml
 		imgflag = (ImageView) itemView.findViewById(R.id.flag);
 		// Capture position and set to the ImageView
-		imgflag.setImageResource(flag[position]);
+		imgflag.setImageResource(mDrawables[position]);
+
+        txtHeader = (TextView) itemView.findViewById(R.id.header_title);
+
+        decorateViews(position);
 
 		// Add viewpager_item.xml to ViewPager
 		((ViewPager) container).addView(itemView);
@@ -75,10 +70,25 @@ public class ViewPagerAdapter extends PagerAdapter {
 		return itemView;
 	}
 
+
+    private void decorateHeaderText(int position){
+        switch (position){
+            case YOUR_NETWORK:
+                String styledText = "<font color='#2f928e'>Your Network, </font> <font color='#e4af1e'><i>Your</i></font><font color='#2f928e'> Dropbox</font>";
+                txtHeader.setText(Html.fromHtml(styledText), TextView.BufferType.SPANNABLE);
+                break;
+        }
+    }
+
+
+    private void decorateViews(int position){
+        decorateHeaderText(position);
+    }
+
 	@Override
 	public void destroyItem(ViewGroup container, int position, Object object) {
 		// Remove viewpager_item.xml from ViewPager
-		((ViewPager) container).removeView((RelativeLayout) object);
+		((ViewPager) container).removeView((ScrollView) object);
 
 	}
 }
